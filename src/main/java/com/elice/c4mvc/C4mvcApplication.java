0c38eb0d32b55d36d0365900dc93744beddb91eb
An-changmin <139744625+An-changmin@users.noreply.github.com>
2024-08-05 14:07:11 +0900

    first commit

---
+++ src/main/java/com/elice/c4mvc/C4mvcApplication.java
@@ -0,0 +1,13 @@
+package com.elice.c4mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class C4mvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(C4mvcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/elice/c4mvc/C4mvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elice.c4mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class C4mvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
